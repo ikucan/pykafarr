8ca16f200d54e640193bd9caf0d4f048d1ce4744
ik <kme@nowhere.org>
2018-12-17 14:46:02 +0000

    interim. BREAKS BUILD!

--- src/cpp/arr_tbl_dcdr.hpp
+++ src/cpp/arr_tbl_dcdr.hpp
@@ -15,6 +15,30 @@ namespace kafarr {
   class arr_tbl_dcdr {
   public:
     static void arr2avr(const std::shared_ptr<arrow::Table> tbl, const std::shared_ptr<Serdes::Schema> schm){
+      /**
+       * get the avro schema from the Serdes schema wrapper
+       */
+      const std::shared_ptr<avro::ValidSchema> avr_schm(schm->object());
+      const avro::NodePtr avr_root_nd = avr_schm->root();
+      
+      /**
+       * make sure that the arrow table schema can satisfy the requirements of the avro schema
+       */
+      auto tbl_schm = tbl->schema();
+      for(int i = 0; i < root->leaves(); ++i){
+	//root->nameAt(i));
+	auto lf = root->leafAt(i);
+	if (!lf) throw kafarr::err("BUG. leaf node is null", root->nameAt(i));
+	std::cerr << " AVRO schema field @" << i << " :: " << root->nameAt(i) << " : " << lf->type() << std::endl;
+	
+	if(tbl_schm->GetFieldByName(root->nameAt(i)))
+	  std::cerr << " table contains schema field: " << root->nameAt(i) << std::endl;
+	else{
+	  std::cerr << " ERROR. schema field is missing: " << root->nameAt(i) << std::endl;
+	  throw kafarr::err("ERROR. schema field is missing: ", root->nameAt(i));
+	}
+      }
+      
       
     }
   };
