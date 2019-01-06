be7f4127f44d515ab8a1c53f0ba530dcc46a7efd
ik <kme@nowhere.org>
2019-01-06 02:18:04 +0000

    ...

--- src/setup.py
+++ src/setup.py
@@ -64,24 +64,71 @@ class build_ext(_build_ext):
         print("INIT OPTS")
         _build_ext.initialize_options(self)
 
+    def mk_incldpth(self):
+        ##py_incl_pth = sys.prefix
+        paths=[]
+        # ptyhon headers
+        paths.append(sysconfig.get_python_inc())
+
+        # pykafarr c++ impl
+        paths.append(os.getcwd() + "/cpp")
+        
+        return ['-I' + pth for pth in paths]
+        
     def _run_cmake(self):
         print("RUN CMAKE")
         # The directory containing this setup.py
         source = os.path.dirname(os.path.abspath(__file__))
-
-        # The staging directory for the module being built
         build_temp = pjoin(os.getcwd(), self.build_temp)
         build_lib = os.path.join(os.getcwd(), self.build_lib)
         saved_cwd = os.getcwd()
 
         if not os.path.isdir(self.build_temp):
             self.mkpath(self.build_temp)
+        if not os.path.isdir(self.build_lib):
+            self.mkpath(self.build_lib)
             
-        print("build_temp:>> " + build_temp)
-        print("self.build_tmp:>> " + self.build_temp)
-        print("build_lib:>> " + build_lib)
-        print("saved_cwd:>> " + saved_cwd)
+        print("---------------------------------------------")
+        print("build_temp     :>>  %s" % build_temp)
+        print("self.build_tmp :>>  %s" % self.build_temp)
+        print("build_lib      :>>  %s" % build_lib)
+        print("saved_cwd      :>>  %s" % saved_cwd)
+        print("python exe     :>>  %s" % sys.executable)
+        print("python env     :>>  %s" % sys.prefix)
+        print("---------------------------------------------")        
 
+        ## ###################################################
+
+        # run the cython compiler to generate the cpp interface
+        self.spawn(['python', '-m',  'cython', '--cplus', '-X', 'language_level=3', 'pykafarr.pyx'])
+
+        # compile the pykafarr source into an object 
+        inclds = self.mk_incldpth()
+        compat_args = ['-B', pjoin(sys.prefix, 'compiler_compat')]
+        cmplr_args = ['-pthread', '-Wl,--sysroot=/', '-std=c++17', '-DNDEBUG', '-fPIC', '-fwrapv', '-O3', '-g']
+# -pthread -B /opt/python/miniconda3/envs/pykafarr/compiler_compat -Wl,--sysroot=/ -Wsign-compare -DNDEBUG -g -fwrapv -O3 -Wall -Wstrict-prototypes -fPIC -I/opt/python/miniconda3/envs/pykafarr/lib/python3.7/site-packages/numpy/core/include -I/opt/python/miniconda3/envs/pykafarr/lib/python3.7/site-packages/pyarrow/include -Icpp/ -I/opt/python/miniconda3/envs/pykafarr/include/python3.7m -c pykafarr.cpp -o build/temp.linux-x86_64-3.7/pykafarr.o -std=c++17 -fPIC -w
+        obj_file = pjoin(build_temp , 'pykafarr.o')
+        cmpl_cmd = ['gcc'] + compat_args + cmplr_args + inclds + [ '-c', 'pykafarr.cpp', '-o', obj_file]
+        print(cmpl_cmd)        
+        self.spawn(cmpl_cmd)
+
+        # link...
+        lnkr_args = ['-pthread', '-shared']
+#g++
+# -pthread
+# -shared
+# -B /opt/python/miniconda3/envs/pykafarr/compiler_compat
+# -L/opt/python/miniconda3/envs/pykafarr/lib
+# -Wl,-rpath=/opt/python/miniconda3/envs/pykafarr/lib
+# -Wl,--no-as-needed
+# -Wl,--sysroot=/
+# build/temp.linux-x86_64-3.7/pykafarr.o
+#-lm -ldl -lpthread -lrt -lstdc++ -lserdes++ -lserdes -lrdkafka++ -lrdkafka -larrow
+#-o build/lib.linux-x86_64-3.7/pykafarr.cpython-37m-x86_64-linux-gnu.so
+        
+        lnk_cmd = ['g++'] + lnkr_args + [obj_file, '-o', pjoin(build_lib, 'pykafarr.so')]
+        self.spawn(lnk_cmd)
+        
     def run(self):
         print("RUN")
         self._run_cmake()
@@ -99,9 +146,7 @@ setup(
     cmdclass={
         'clean': clean,
         'build_ext': build_ext
-},
-
-
+    },
     
 )
 
