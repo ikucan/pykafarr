0ba9699addaabfe74053ff7b6126ace09e1faf4b
iztok <ik@nowhere.org>
2019-01-10 00:14:26 +0000

    ...

--- conda/build.sh
+++ conda/build.sh
@@ -15,8 +15,10 @@ cp -d $SERDES_LIBS_PATH/libserdes*    $PREFIX/lib
 cp -d $KAFKA_LIBS_PATH/librdkafka*    $PREFIX/lib
 cp -d $AVRO_LIBS_PATH/libavro*        $PREFIX/lib
 cp -d $JANSSON_LIBS_PATH/libjansson*  $PREFIX/lib
-cp -d $LIBCURL_LIBS_PATH/libcurl*  $PREFIX/lib
+cp -d $LIBCURL_LIBS_PATH/libcurl*     $PREFIX/lib
 echo "+++++++++++++++++++++++++++++++++++++++++"
+
+exit 1
 
 ${PYTHON} setup.py install || exit 1
 
