ef06591238078b56dd4e83f34e4a00eec1be3607
iztok <ik@nowhere.org>
2019-01-15 02:22:55 +0000

    add a test install docker image

--- dckr/bld_env/build.sh
+++ dckr/bld_env/build.sh
@@ -4,4 +4,4 @@ rm -Rf pykafarr
 git clone https://github.com/ikucan/pykafarr.git
 cd pykafarr/conda
 conda-build --no-remove-work-dir .
-anaconda upload --force /opt/miniconda/conda-bld/linux-64/pykafarr-0.7.0-py37_0.tar.bz2
+#anaconda upload --force /opt/miniconda/conda-bld/linux-64/pykafarr-0.7.0-py37_0.tar.bz2
